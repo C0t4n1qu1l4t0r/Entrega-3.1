c19ff440694b170c81085af1b825688bdb3c2664
Razvan Cotan <razvancotan0@gmail.com>
2023-04-17 10:45:37 +0200

    Entrega 3.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './prueba/prueba.component';
+import { Prueba2Component } from './prueba2/prueba2.component';
+import { FirmaComponent } from './firma/firma.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LibrosComponent } from './libros/libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    Prueba2Component,
+    FirmaComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
